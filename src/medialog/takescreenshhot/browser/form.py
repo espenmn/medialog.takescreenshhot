17100057b4e74403d633ad80acb9b1a2980c79f4
Espen Moe-Nilssen <espen@medialog.no>
2019-06-26 17:26:30 +0200

    first working version with lots of stuff to remove (duplicates)

--- src/medialog/takescreenshhot/browser/form.py
+++ src/medialog/takescreenshhot/browser/form.py
@@ -1,56 +1,121 @@
 #import urllib.request
+import re
 import requests
 import gzip
 from plone import api
 from plone.supermodel import model
 from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.statusmessages.interfaces import IStatusMessage
+from z3c.form import form
+from z3c.form import field
 from z3c.form import button
 from zope import schema
 from zope.interface import alsoProvides
+
+from plone.autoform.form import AutoExtensibleForm
 
 from zope.i18nmessageid import MessageFactory
 
 _ = MessageFactory('medialog.takescreenshhot')
 
-class ScreenshotForm(BrowserView):
-    """ Define Form handling"""
-
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
-
-    def __call__(self):
-        context = self.context
-        folder_name = self.context.Title()
-        my_urls = api.content.find(context=context)
-        return self.make_screenshot(folder_name, my_urls)
-
-    def make_screenshot(self, folder_name, my_urls):
-        pages_done = []
-        #call one and one page until I find out how to do this the right way
-        for my_url in my_urls:
-            obj = my_url.getObject()
-            page = obj.absolute_url().replace(" ", "")
-            page_name = obj.Title.replace(" ", "")
-            my_url = "http://localhost:5010/single_screenshot/%s/%s/%s" % (folder_name, page_name, page)
-            response = requests.post(my_url)
-            pages_done.append(response)
-
-        return 1
 
 
-    # def __call__(self):
-    #     context = self.context
-    #     my_urls = api.content.find(context=context)
-    #     import pdb; pdb.set_trace()
-    #     #page = self.context.absolute_url()
-    #     my_url = "http://localhost:5010/screenshots"
-    #     data = '''{
-    #       "query": {
-    #             "folder_name": 'my_folder_with_images',
-    #             "pages": [['http://medialog.no', 'image1.png'], ['http://plone.org', 'image2.png']
-    #         }'''
-    #
-    #     response = requests.post(my_url, data)
-    #     return response
+
+class IScreenshotForm(model.Schema):
+     """ Define form fields """
+
+     names = schema.ASCII(
+                title=u"URLs",
+                description=u"One on each line. For sitemap: Only base URL ( http://medialog.no )",
+                required=False
+          )
+
+
+class ScreenshotForm(AutoExtensibleForm, form.Form):
+    """ Make screenshots (png)"""
+
+    schema = IScreenshotForm
+    #template = ViewPageTemplateFile("screentshot.pt")
+    ignoreContext = True
+    success = False
+
+    @button.buttonAndHandler(u'This page')
+    def handleApply(self, action):
+        #send url to (make) single screenshot
+        url = self.context.absolute_url()
+        return self.single_screensthot(url)
+
+    @button.buttonAndHandler(u'Sub content')
+    def handleApply(self, action):
+        context=self.context
+        #all content below here
+        #consider using 'depth' to limit
+        kontents = api.content.find(context)
+        for kontent in kontents:
+            url = kontent.getObject().absolute_url()
+            #print url
+            self.single_screensthot(url)
+            IStatusMessage(self.request).addStatusMessage(url, "info")
+
+    @button.buttonAndHandler(u'URL list')
+    def handleApply(self, action):
+        #make screenshots of (text)list of urls
+        my_data = self.extractData()
+        if my_data:
+            shots = []
+            folder = 'screenshots'
+            pages = my_data[0]['names']
+            urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', pages)
+            if urls:
+                for url in urls:
+                    name = self.single_screensthot(url)
+                    IStatusMessage(self.request).addStatusMessage(name, "info")
+        IStatusMessage(self.request).addStatusMessage('Please check your input', "warning")
+
+    @button.buttonAndHandler(u'Sitemap')
+    def handleApply(self, action):
+         #make screenshots of all in Plone sitemap
+         my_data = self.extractData()
+         if my_data:
+             pages = my_data[0]['names']
+             urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', pages)
+             if urls:
+                 for url in urls:
+                     my_url = "http://localhost:5010/sitemap/%s" % url
+                     response = requests.post(my_url)
+                     image_path = "One day, maybe avalable at http://localhost:5010/get_files/%s " % response.text
+                     IStatusMessage(self.request).addStatusMessage(
+                        image_path, "info"
+                     )
+         IStatusMessage(self.request).addStatusMessage(
+                'Please check your input', "warning"
+         )
+
+    def make_screensthot(self, my_urls, folder_name):
+         pages = []
+         for my_url in my_urls:
+              page = my_url.getObject().absolute_url()
+              page_name = my_url.getObject().title
+              my_url = "http://localhost:5010/single_screenshot/%s/%s/%s" % (folder_name, page_name, page)
+              response = requests.post(my_url)
+              pages.append(response.text)
+         return pages
+
+    def single_screensthot(self, page):
+         folder_name = 'screenshots'
+         page_name = page.replace("http://", "")
+         page_name = page_name.replace("https/::", "-")
+         page_name = page_name.replace(".", "-")
+         page_name = page_name.replace(":", "-")
+         page_name = page_name.replace("/", "-")
+         my_url = "http://localhost:5010/single_screenshot/%s/%s/%s" % (folder_name, page_name, page)
+         response = requests.post(my_url)
+         image_path = "Image soon avalable at http://localhost:5010/get_file/%s " % page_name
+         IStatusMessage(self.request).addStatusMessage(
+                    image_path, "info"
+         )
+         #return response
+
+    #def updateActions(self):
+    #    self.actions[u"Sitemap].addClass("black")
